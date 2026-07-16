01f3fd5ef7e7ec85d48d8c8b5a1a4c32c38343cb
CoaderVikas <vikas.yadav83499@gmail.com>
2026-07-16 22:08:58 +0530

    added sl4j

--- src/main/java/com/mailer/aspect/LoggingAspect.java
+++ src/main/java/com/mailer/aspect/LoggingAspect.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Description: this class is responsible for logging method execution
  * Author     : Vikas Yadav
@@ -18,10 +20,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Slf4j
 public class LoggingAspect {
 
-	private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
-	
 	@Around(
 	         "execution(* com.mailer.service..*(..)) ||"+
     		 "execution(* com.mailer.config..*(..)) ||"+
